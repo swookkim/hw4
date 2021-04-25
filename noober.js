f247f98a5068de83984afbf4c795a4e9cc3011b5
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-04-25 12:25:24 -0500

    1

--- noober.js
+++ noober.js
@@ -18,17 +18,52 @@ window.addEventListener('DOMContentLoaded', async function() {
     let passengerNumber = rideRequest[i].numberOfPassengers
     let dropoffLocation = rideRequest[i].dropoffLocation
     let pickupLocation = rideRequest[i].pickupLocation
-    let purpleRequest = rideRequest[i].purpleRequested
+    let serviceLevel = if (rideRequest[i].purpleRequested == true){
+      serviceLevel = `Noober Purple`
+      } else if(passengerNumber>3){
+      serviceLevel = `Noober XL`  
+      } else {
+      serviceLevel = `Noober X`
+      }
 
     // Create a variable for HTML element that we will add to the page
     let element = document.querySelector(`.rides`)
 
     // Insert HTML into the page, using the product data
-    element.insertAdjacentHTML(`beforeend`,
-    
-    
+    element.insertAdjacentHTML(`beforeend`,`
+    <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+      <i class="fas fa-car-side"></i>
+      <span>Noober X</span>
+    </h1>
 
+    <div class="border-4 border-gray-900 p-4 my-4 text-left">
+      <div class="flex">
+        <div class="w-1/2">
+          <h2 class="text-2xl py-1">Foghorn Leghorn</h2>
+          <p class="font-bold text-gray-600">(312) 555-1212</p>
+        </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+            2 passengers
+          </span>
+        </div>
+      </div>
+      <div class="mt-4 flex">
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">PICKUP</div>
+          <p>123 Main St</p>
+          <p>Chicago, IL 60603</p>
+        </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+          <p>123 Main St</p>
+          <p>Chicago, IL 60603</p>
+        </div>
+      </div>
+    </div>`
+    
     )
+    
   }
   )}
 
