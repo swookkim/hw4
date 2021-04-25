b8b1937aaf14e7af09cd9af338046a6fdbaadbff
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-04-25 12:13:21 -0500

    1

--- noober.js
+++ noober.js
@@ -8,15 +8,27 @@ window.addEventListener('DOMContentLoaded', async function() {
   // 🔥 start here: write the recipe (algorithm), then write the code
   
   // Create a variable for product data
-
   let rideRequest = json
-  console.log(rideRequest)
 
   // Loop through the data
+  for (i=0, i<rideRequest.length, i++){
 
     // Create a variable to store each data point
+    let passengerDetail = rideRequest[i].passengerDetail
+    let passengerNumber = rideRequest[i].numberOfPassengers
+    let dropoffLocation = rideRequest[i].dropoffLocation
+    let pickupLocation = rideRequest[i].pickupLocation
+    let purpleRequest = rideRequest[i].purpleRequested
 
     // Create a variable for HTML element that we will add to the page
+    let element = document.querySelector(`.rides`)
 
-    // Insert HTML into the page, using the product data 
-})
+    // Insert HTML into the page, using the product data
+    element.insertAdjacentHTML(`beforeend`,
+    
+    
+
+    )
+  }
+  )}
+
